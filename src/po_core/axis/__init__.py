17c442e3cdf90bf5830c1cddf9abbf1add574677
flying_pig <tanakantyo0229@gmail.com>
2026-03-02 00:53:31 +0900

    Add deterministic axis scoring for synthesis report (#283)

--- src/po_core/axis/__init__.py
+++ src/po_core/axis/__init__.py
@@ -1,5 +1,6 @@
-"""Decision axis related models."""
+"""Decision axis related models and utilities."""
 
+from .scoring import score_text
 from .spec import AxisDimension, AxisSpec, load_axis_spec
 
-__all__ = ["AxisDimension", "AxisSpec", "load_axis_spec"]
+__all__ = ["AxisDimension", "AxisSpec", "load_axis_spec", "score_text"]
